8e8e886a1c9802776f24f9d4d97fa0179b68775d
youngminh1303 <100891209+youngminh1303@users.noreply.github.com>
2022-03-08 16:38:21 +0700

    fix bugs + add new information board

--- script.js
+++ script.js
@@ -3,8 +3,12 @@ const restartButton = document.getElementById("btnRestart");
 const startButton = document.getElementById("btnStart");
 const playerOneName = document.querySelector("#playerOneName");
 const playerTwoName = document.querySelector("#playerTwoName");
+const infomation = document.querySelector("#information");
+
+
 let winner;
 let currentMark = "X";
+let  playWithBot = false;
 
 const gameBoard = (() => {
     let board =  
@@ -30,7 +34,7 @@ const findTheWinner = (row, position, PlayerOne, PlayerTwo) => {
     console.log(PlayerTwo);
     let win = false;
     let winnerMark;
-    //check if draw 
+
   
     //check on row
     if(gameBoard.board[row][0] === gameBoard.board[row][1] && gameBoard.board[row][0] === gameBoard.board[row][2]) {
@@ -48,13 +52,13 @@ const findTheWinner = (row, position, PlayerOne, PlayerTwo) => {
     }
     //check on diag
     if(gameBoard.board[2][0] === gameBoard.board[1][1] && gameBoard.board[2][0] === gameBoard.board[0][2]) {
-        if (gameBoard.board[2][2] !== ""){
+        if (gameBoard.board[2][0] !== ""){
             winnerMark = gameBoard.board[2][2];
             win = true;
             }
     }
     //check on columns
-    if(gameBoard.board[0][position] === gameBoard.board[1][position] && gameBoard.board[row][position] === gameBoard.board[2][position]){
+    if(gameBoard.board[0][position] === gameBoard.board[1][position] && gameBoard.board[0][position] === gameBoard.board[2][position]){
         if (gameBoard.board[0][position] !== ""){
             winnerMark = gameBoard.board[0][position];
             win = true;
@@ -63,26 +67,29 @@ const findTheWinner = (row, position, PlayerOne, PlayerTwo) => {
 
     if (!gameBoard.board[0].includes("") && !gameBoard.board[1].includes("") && !gameBoard.board[2].includes(""))
     {
-        alert("DRAW");
+        information.textContent = `DRAW`;
         return 0;
     }
 
     //winner alert
     if (win) {
         if (PlayerOne.mark === winnerMark) {
-            alert(`${PlayerOne.name} wins`);
+            information.textContent = `${PlayerOne.name} wins`;
             return 1;
         }
 
-        alert(`${PlayerTwo.name} wins`);
+        information.textContent = `${PlayerTwo.name} wins`;
         return 2;
     }
 };
 
 
-const changePlayer = (currentMark, PlayerOneMark, PlayerTwoMark) => {
-    if (currentMark === PlayerOneMark) return PlayerTwoMark;
-    return PlayerOneMark;
+const changePlayer = (PlayerOneMark, PlayerTwoMark) => {
+    if (currentMark === PlayerOneMark) {
+        currentMark = PlayerTwoMark;
+        return;
+    };
+    currentMark = PlayerOneMark;
 };
 
 
@@ -99,11 +106,11 @@ const render = (PlayerOne, PlayerTwo) => {
           square.addEventListener("click", ()=> {
               //Do nothing if the square already marked
               if (winner) return
-              if (square.textContent === " ") return
+              if (square.textContent !== "") return
               square.textContent = currentMark;
               gameBoard.board[row][id] = currentMark;
               winner = findTheWinner(row, id, PlayerOne, PlayerTwo);
-              currentMark = changePlayer(currentMark, PlayerOne.mark, PlayerTwo.mark);
+              changePlayer(PlayerOne.mark, PlayerTwo.mark);
           });
     
           theBoard.appendChild(square);
@@ -123,14 +130,29 @@ const reset = (PlayerOne, PlayerTwo) => {
         while (theBoard.firstChild) {
             theBoard.removeChild(theBoard.lastChild)
         }
-        render(PlayerOne, PlayerTwo)
+        winner = false;
+        currentMark = "X";
+        render(PlayerOne, PlayerTwo);
+        infomation.textContent = "Playing"
     }
     
+
+
     startButton.addEventListener("click", () => {
+        //create 2 players
+        if (!playerOneName.value || !playerTwoName.value) {
+            alert("Please type two players's names");
+            return
+        }
         const PlayerOne = Player(playerOneName.value, "X");
         const PlayerTwo = Player(playerTwoName.value, "O");
+        //set event for the restart button, do it in start button because the restart only shows when we press the start button
+        restartButton.addEventListener("click", () => {
+            reset(PlayerOne, PlayerTwo)
+        })
         reset(PlayerOne, PlayerTwo)
-        restartButton.classList.remove("hide")
+        restartButton.classList.remove("hide");
+        infomation.classList.remove("hide");
 
     });
     
@@ -140,6 +162,6 @@ const reset = (PlayerOne, PlayerTwo) => {
     
     
     //reset button 
-    restartButton.addEventListener("click", reset)
+   
 
 
